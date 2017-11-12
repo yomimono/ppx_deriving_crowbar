d467872086be3593d3e7658edbaed9a27b22da4a
Mindy Preston <meetup@yomimono.org>
2017-11-12 15:59:46 -0600

    now with tuples???

--- src/ppx_deriving_crowbar.ml
+++ src/ppx_deriving_crowbar.ml
@@ -56,21 +56,24 @@ let str_of_type ~options ~path ({ptype_loc = loc } as type_decl) =
           (* under what circumstances can pcd_res be non-None and pcd_args be
              populated? *)
           match pcd_res, pcd_args with
-          | None, Pcstr_tuple l ->
-            let gens = List.map (expr_of_typ quoter) l in
-            (* we're going to need some slick dancing to deal with this
-               arity situation I think *)
-            [%expr Crowbar.(map [%e gens]
-                              (fun (* output n space-separated unique variable
-                                      names, where n is the length of the tuple
-                                      list *)
-                                -> (* output those same unique variables, but
-                                      separated by ,s *)
-                ()))]
-          | None, Pcstr_tuple [] ->
-            (* C of T1 * ... * Tn *)
-            (* but we have no information on t1, ..., tn :( *)
-            [%expr Crowbar.const ()]
+          | None, Pcstr_tuple tuple ->
+            let rec n_vars n (l : string list) =
+              if n > 0 then
+                n_vars (n-1) ((Ppx_deriving.fresh_var l)::l)
+              else
+                List.rev l
+            in
+            (* make a tuple of those names *)
+            let desc =
+              (List.map
+                 (fun i -> Ast_helper.Exp.mk @@
+                   Pexp_ident (Ast_convenience.lid i))
+                 (n_vars (List.length tuple) []))
+            in
+            Ast_convenience.tuple desc
+            (* TODO: wrap that tuple in `fun a b c d`
+            let gens = List.map (expr_of_typ quoter) tuple in
+            [%expr Crowbar.(map [%e gens] [%e fn])] *)
           | Some core_type, Pcstr_tuple _ | Some core_type, Pcstr_record _ ->
             (* C: T0  or C: T1 * ... * Tn -> T0 or C: {...} -> T0 *)
             expr_of_typ quoter core_type (* 
