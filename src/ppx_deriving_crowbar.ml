cfad10876eaee191af3a759c71213981da4feea3
Mindy Preston <meetup@yomimono.org>
2017-11-12 18:14:50 -0600

    function!

--- src/ppx_deriving_crowbar.ml
+++ src/ppx_deriving_crowbar.ml
@@ -76,9 +76,9 @@ let str_of_type ~options ~path ({ptype_loc = loc } as type_decl) =
             (* easiest case : how do we get "fun b -> A (a, b)" ?  then we can
                add "fun a -> " etc around it. *)
             let last_fun v = Ast_helper.Exp.fun_ Nolabel None
-                (Ast_helper.Pat.var (Location.mknoloc v)))
+                (Ast_helper.Pat.var (Location.mknoloc v))
                 res in
-            last_fun
+            last_fun (List.(hd @@ rev vars))
             (* TODO: wrap that tuple in `fun a b c d` *)
             (* let gens = List.map (expr_of_typ quoter) tuple in
             [%expr Crowbar.(map [%e gens] [%e last_fun])] *)
