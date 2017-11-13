66c4eaefe9248738784da4b09a62002ff3022923
Mindy Preston <meetup@yomimono.org>
2017-11-13 13:41:40 -0600

    try it out a little more

--- test/test.ml
+++ test/test.ml
@@ -1,2 +1,10 @@
 type foo = A of int | B of float | C of int * float
 [@@deriving crowbar]
+
+let () =
+  Crowbar.(add_test ~name:"everything is awesome"
+             [generate_foo] (fun foo -> check @@ match foo with
+      | A i -> Printf.printf "an int: %d\n%!" i; true
+      | B f -> Printf.printf "a float: %f\n%!" f; true
+      | C (i, f) -> (Printf.printf "OMG, an int AND a float: %d %f\n%!" i f); true)
+          )
