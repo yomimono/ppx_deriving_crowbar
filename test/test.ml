916ab1e4d4afcd430ca6d806f373985c5eb4ccf0
Mindy Preston <meetup@yomimono.org>
2017-11-13 15:13:24 -0600

    test with records too, oh no

--- test/test.ml
+++ test/test.ml
@@ -1,10 +1,16 @@
 type foo = A of int | B of float | C of int * float
 [@@deriving crowbar]
 
+type bar = {
+  justice: bool;
+  purrs: int;
+}
+[@@deriving crowbar]
+(* 
 let () =
   Crowbar.(add_test ~name:"everything is awesome"
              [generate_foo] (fun foo -> check @@ match foo with
       | A i -> Printf.printf "an int: %d\n%!" i; true
       | B f -> Printf.printf "a float: %f\n%!" f; true
       | C (i, f) -> (Printf.printf "OMG, an int AND a float: %d %f\n%!" i f); true)
-          )
+          ) *)
