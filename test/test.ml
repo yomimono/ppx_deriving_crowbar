0854305673d87139f5bd84738949801c39063faf
Mindy Preston <meetup@yomimono.org>
2017-11-15 16:07:55 -0600

    add some invocations to test file

--- test/test.ml
+++ test/test.ml
@@ -12,9 +12,10 @@ type bar = {
   rejweo: quux;
 }
 [@@deriving crowbar]
+
 let () =
   Crowbar.(add_test ~name:"everything is awesome"
-             [generate_foo] (fun foo -> check @@ match foo with
+             [generate_foo; generate_bar; generate_quux] (fun foo _bar _quux -> check @@ match foo with
       | A i -> Printf.printf "an int: %d\n%!" i; true
       | B f -> Printf.printf "a float: %f\n%!" f; true
       | C _ -> (Printf.printf "OMG, something exotic!\n%!"); true)
