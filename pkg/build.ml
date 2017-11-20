ad94463d60c1fe00c6e1affac297c78929c92301
Mindy Preston <meetup@yomimono.org>
2017-11-20 16:24:04 -0600

    Revert "remove ppx_deriving_crowbar_runtime"
    This reverts commit 71e30929d4587f22b8b6bd74abdea6eddf90ab6c.

--- pkg/build.ml
+++ pkg/build.ml
@@ -9,6 +9,7 @@ let () =
   Pkg.describe "ppx_deriving_crowbar" ~builder:(`Other (ocamlbuild, "_build")) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.library "src/ppx_deriving_crowbar";
+    Pkg.lib ~exts:Exts.module_library "src/ppx_deriving_crowbar_runtime";
     Pkg.doc "README.md";
     Pkg.doc "LICENSE.txt";
     Pkg.doc "CHANGES.md"; ]
